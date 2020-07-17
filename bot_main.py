48b4103db8931b4fa3ffd571580573686d7e28c6
Bùi Tiến Dũng <46742676+LiquidRekto@users.noreply.github.com>
2020-07-17 15:22:05 +0700

    update ask function

--- bot_main.py
+++ bot_main.py
@@ -10,6 +10,15 @@ async def on_message(message):
    else:
       if message.content.startswith("WASSUP"):
          await message.channel.send(f"WASSUP {message.author.mention}")
+      if message.content.startswith("$ask"):
+         ctx = message.content.split()
+         ask_channel = client.get_channel("733594602287792149")
+         question = ""
+         for question_chunk in ctx[1:]:
+            question += question_chunk
+               if (ctx[1:].index(question_chunk) < len(ctx[1:]) - 1):
+                   question += " "
+          await client.send_message(ask_channel, f"Người dùng tên **{message.author}** đã hỏi: *{question}*")
 
 
 
