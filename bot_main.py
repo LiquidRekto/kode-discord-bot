015cd4975d337316269bfc4c8fdcf57cdbccd389
LiquidRekt <dung6ctd@gmail.com>
2020-07-17 15:47:40 +0700

    update ask

--- bot_main.py
+++ bot_main.py
@@ -18,7 +18,7 @@ async def on_message(message):
             question += question_chunk
             if (ctx[1:].index(question_chunk) < len(ctx[1:]) - 1):
                question += " "
-         await ask_channel.send(ask_channel, f"Người dùng tên **{message.author}** đã hỏi: *{question}*")
+         await ask_channel.send(f"Người dùng tên **{message.author}** đã hỏi: *{question}*")
 
 
 
